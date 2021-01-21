9c8b90be750940051d069514f9f1768c01bb0b20
Jonatan <jonatan.kruszewski@itc.tech>
2021-01-21 23:45:15 +0200

    Cleaned imports in animation.js

--- js_modules/animation.js
+++ js_modules/animation.js
@@ -1,6 +1,3 @@
-import { grabElement } from "./handler.js";
-//import * as gsap from "./gsap.min.js";
-
 const animate = (nav, body) => {
   const tl = gsap.timeline({ defaults: { duration: 1 } });
   tl.from(nav, { y: -100, stagger: 0.6, opacity: 1 }).from(body, {
